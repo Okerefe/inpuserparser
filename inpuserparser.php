86d795e4418aadbb3e1c430ae06e5a03b8af1060
Okerefe <okerefe@gmail.com>
2020-05-31 03:18:50 +0100

    Project Initialzation
    Initialize Project with Boiler Plate files and folders
    Add Composer and project dependencies

---
+++ inpuserparser.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: InpUserParser
+Description: Parse and Perform Different Search Transactions on a Custom Users Class.
+Plugin URI:  https://github.com/
+Author: Umukoro Okerefe
+Author URI: https://deravenedwriter.com/
+Author:      Umukoro Okerefe
+Version:     1.0
+*/
+
+// Exit if File is called Directly
+if (!defined('ABSPATH')) {
+    exit;
+}
